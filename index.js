26becca3ad567cd5ff276f7d03373bb186c89f8e
sotiris <sotirismpalas@gmail.com>
2022-11-12 12:17:16 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const axios = require("axios");
+const fetch = require("cross-fetch");
+
+const IP_CHECK_INTERVAL = 1000 * 60 * 5; // interval to check the ip
+
+const headers = {
+  "X-Auth-Email": process.env.xAuthEmail,
+  "X-Auth-Key": process.env.xAuthKey,
+  "Content-Type": "application/json",
+};
+
+async function sleep(t) {
+  return new Promise((r) => setTimeout(r, t));
+}
+
+(async () => {
+  console.log("Started");
+  while (true) {
+    try {
+      let resp = await axios.get("https://api.ipify.org");
+      if (resp.status && resp.status === 200) {
+        const myIp = resp.data;
+        resp = await axios.get(
+          `https://api.cloudflare.com/client/v4/zones/${process.env.zoneId}/dns_records/${process.env.recordId}`,
+          { headers }
+        );
+        if (resp.status && resp.status === 200) {
+          const cloudFlareIp = resp.data.result.content;
+          if (myIp !== cloudFlareIp) {
+            resp = await axios.put(
+              `https://api.cloudflare.com/client/v4/zones/${process.env.zoneId}/dns_records/${process.env.recordId}`,
+              {
+                type: "A",
+                name: "7ps.xyz",
+                content: myIp,
+                ttl: 3600,
+                proxied: true,
+              },
+              { headers }
+            );
+            resp.data.success
+              ? console.log(`IP changed successfully to: ${myIp}`)
+              : console.log(`Something went wrong changing the IP`);
+          }
+        }
+      }
+    } catch (err) {}
+
+    await sleep(IP_CHECK_INTERVAL);
+  }
+})();
